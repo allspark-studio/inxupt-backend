d79001c43fb605c9c1f3c6dae354c4ff552f91c2
chanshy <362774405@qq.com>
2020-11-22 20:31:14 +0800

    2020/11/22 20:31 陈帅 0.1.66：修改上传文件路径为固定文件夹 0.1.67：修改部分需要token的接口 0.1.68：修复取消点赞餐品的bug 0.1.69：修复取消点赞总共被赞数不减的bug 0.1.70：修复圈子关注人数显示不正确的bug

--- src/main/java/com/allsparkstudio/zaixiyou/enums/AddExpEnum.java
+++ src/main/java/com/allsparkstudio/zaixiyou/enums/AddExpEnum.java
@@ -4,16 +4,27 @@ import lombok.Getter;
 
 @Getter
 public enum AddExpEnum {
+    // 每日登陆
     LOGIN(2),
+    // 被点赞
     GET_LIKE(1),
+    // 被收藏
     GET_FAVORITE(2),
+    // 被评论
     GET_COMMENT(2),
+    // 被关注
     GET_FOLLOWED(3),
+    // 被投币
     GET_COIN(2),
+    // 发帖子/文章
     ADD_POST(3),
+    // 发评论
     ADD_COMMENT(2),
+    // 新建圈子
     ADD_CIRCLE(5),
+    // 发布公告
     ADD_ANNOUNCEMENT(3),
+    // 投币
     INSERT_COIN(3);
 
     Integer exp;
